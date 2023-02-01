04e5e5f9ddb304b10f5037e3a6a33aca624b715c
LimSeJin9577 <goodoo9577@gmail.com>
2023-02-01 13:51:08 +0900

    Backend work complete

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -2,6 +2,9 @@ import os.path
 import uuid
 from pathlib import Path
 import markdown
+from glob import glob
+import cv2
+import numpy as np
 
 def create_workspace():
     """
@@ -34,4 +37,26 @@ def openfile(filename):
     }
     return data
 
+def clean_src_path() :
+    src_files = glob("static/resources/temp_src/*")
+    save_files = glob("static/resources/temp_save/*")
+    for f in src_files : 
+        os.remove(f)
+    for sf in save_files :
+        os.remove(sf)
+
+def get_temp_save(name) :
+    return glob(f"static/resources/temp_save/{name}*")
+
+def save_images(image) :
+    request_id = Path(str(uuid.uuid4())[:8])
+    name = f"temp{request_id}"
+    file_path = f"static/resources/temp_src/{name}.jpg"
+    cv2.imwrite(file_path, image)
+    return name
+
+def toImage(image_bytes) :
+    img = np.fromstring(image_bytes, dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    return img
 
--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
-from fastapi import FastAPI, Request, UploadFile
-from fastapi.responses import HTMLResponse
+# 실행방법 : uvicorn app.main:app --reload port 8080
+
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .library.helpers import *
-from app.routers import unsplash, twoforms, accordion
 from typing import List
 from pydantic.main import BaseModel
+import subprocess
 
-# uvicorn app.main:app --reload port 8080
+from app.library.helpers import toImage, save_images
+from app.library.helpers import clean_src_path, get_temp_save
 
 app = FastAPI()
 
 # Jinja문법, templates폴더에 연결
 templates = Jinja2Templates(directory = "templates")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# app.include_router(unsplash.router)
-# app.include_router(twoforms.router)
-# app.include_router(accordion.router)
-
-# Uploaded Image *Response* Form Data
 class ImagePaths(BaseModel) :
     img_path_list : List[str]
 
@@ -30,34 +26,56 @@ class ImagePaths(BaseModel) :
 async def home(request: Request):
     return templates.TemplateResponse("body_reshape.html", {"request": request})
 
-'''
-    Upload 1 Image Post Function
-    > Request <
-    - just 1 image file
 
-    > Response <
-    - [Images/Image_paths] image file 20~30 output with multi-scaled
-'''
+# Complete
 @app.post("/upload_image", response_model=ImagePaths)
 async def upload_image(request: Request, image : UploadFile) :
-    # calc 20~30 images.
+    # init files
+    clean_src_path()
+    
+    # image upload
+    image_bytes = await image.read()
+    image = toImage(image_bytes)
+
+    # temp_files, save_directory
+    name = save_images(image)
+
+    # model inference
+    inference(mode="image", degree=0.0)
+
+    # output_file_path
+    img_path_list = get_temp_save(name)
+    img_path_list.sort()
 
     return ImagePaths(
-        img_path_list = []
+        img_path_list = img_path_list
     )
 
-
-'''
-    Upload Files Post Function
-    > Request <
-    - folder with images
-    - degree(body_reshape_model)
-
-    > Response < 
-    - [Image/Image_paths]Image Folder with guided-scaling Images
-'''
+# Complete
 @app.post("/upload_files", response_model=ImagePaths)
-async def upload_image(request: Request, files : List[UploadFile]) :
+async def upload_files(request: Request, degree : float, files : List[UploadFile] = File(...)) :
+    # init files
+    clean_src_path()
+    name_list = []
+    img_path_list = []
+
+    # save multi-contents(images)
+    for i,file in enumerate(files) :
+        contents = await file.read()
+        image = toImage(contents)
+        name_list.append(save_images(image))
+
+    inference(mode="files", degree=degree)
+    for name in name_list :
+        img_path_list.append(get_temp_save(name))
+    img_path_list.sort()
+
     return ImagePaths(
-        img_path_list = []
+        img_path_list = img_path_list
     )
+
+# Operate Model Inference.py
+def inference(mode="", degree=0.0) :
+    subprocess.run([f"/home/sejin/gitfile/body_reshape_api/app/library/body_reshape/model.sh {mode} {degree}"], shell=True)
+
+
